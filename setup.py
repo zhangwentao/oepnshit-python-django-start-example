aceb5af7ba709bd4120b8ecf88b2611140c68e81
zhangwentao <taowenzhang@gmail.com>
2014-11-16 17:20:18 +0800

    set some setup fields

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='wedate',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Wentao Zhang',
+      author_email='taowenzhang@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Django<=1.6.7'],
      )
