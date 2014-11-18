8a29ad5a9d14e88f0ca8349db4cf5b0f4534d22d
zhangwentao <taowenzhang@gmail.com>
2014-11-18 23:58:47 +0800

    code structure test

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os,sys
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-sys.path.append(os.getcwd()+'/sources/')
+sys.path.append('/var/lib/openshift/5454decb4382ec63e1000319/app-deployments/current/repo/sources')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
